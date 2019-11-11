cd6e98ee1f753e8cf1fa70e935d45abd1dee642f
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-11-12 00:20:36 +0530

    basic http authentication in memory

--- src/main/java/com/belfazt/security/SecurityConfig.java
+++ src/main/java/com/belfazt/security/SecurityConfig.java
@@ -19,14 +19,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.inMemoryAuthentication()
 			.withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
 			.and()
-			.withUser("mani").password(passwordEncoder().encode("mani123")).roles("USER");
+			.withUser("mani").password(passwordEncoder().encode("mani123")).roles("USER")
+			.and()
+			.withUser("maxi").password(passwordEncoder().encode("maxi123")).roles("MANAGER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests()
+			/*.authorizeRequests()
 			.anyRequest().authenticated()
+			.and()
+			.httpBasic(); //Basic html auth*/
+			
+			.authorizeRequests()
+			.antMatchers("/index.html").permitAll()
+			.antMatchers("/profile/**").authenticated()
+			.antMatchers("/admin/**").hasRole("ADMIN")
+			.antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
 			.and()
 			.httpBasic();
 	}
